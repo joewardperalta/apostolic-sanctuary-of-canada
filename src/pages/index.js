84f7cb80180eda2f6f15717fa30004afa3d831b3
Joeward Peralta <joeward_peralta@outlook.com>
2024-02-20 11:52:12 -0500

    Enhanced the style for the hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,17 @@ export default function Home() {
       <section id="hero-section">
         <Container id="hero-description">
           <Row>
-            <Col md={12} lg={6}>
+            <Col md={12} lg={7}>
               <h1 className="title">
                 Jesus is the <span className="bold big-text">Way</span> the{" "}
                 <span className="bold big-text">Truth</span> and the{" "}
                 <span className="bold big-text">Life</span>.
               </h1>
               <p className="subtitle">
-                Welcome to The Apostolic Sanctuary of Canada.
+                Join us at The Apostolic Sanctuary of Canada Church as we praise
+                and worship our one true God Jesus Christ.
               </p>
-              <Button variant="primary">
+              <Button variant="secondary">
                 <Link href="/ministries-and-services">
                   Visit our church
                   <i className="fa-solid fa-arrow-right"></i>
