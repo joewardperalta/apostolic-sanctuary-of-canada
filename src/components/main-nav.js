21a2449d1d9cd3259173283b00870b9abddc0568
Joeward Peralta <jperalta11@seneca.ca>
2024-03-05 01:23:11 -0500

    Add offcanvas to the navbar

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -1,27 +1,41 @@
-import { Container, Nav, Navbar, NavDropdown, Button } from "react-bootstrap";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Button,
+  Offcanvas,
+} from "react-bootstrap";
 
 export default function MainNav() {
   return (
-    <Navbar expand="lg">
+    <Navbar expand="md">
       <Container>
         <Navbar.Brand href="/">
           <img className="logo" src="/church-logo.png" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-menu" />
-        <Navbar.Collapse id="navbar-menu">
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <NavDropdown title="Gatherings" id="gatherings-nav-dropdown">
-              <NavDropdown.Item href="/ministries-and-services">
-                Ministries & Services
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/events">Events</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="/location">Location</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-md`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+          placement="start"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>
+              <img className="logo" src="/church-logo.png" />
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body className="d-flex align-items-center">
+            <Nav className="justify-content-end flex-grow-1 text-center">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/ministries-and-services">Services</Nav.Link>
+              <Nav.Link href="/events">Events</Nav.Link>
+              <Nav.Link href="/location">Location</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
